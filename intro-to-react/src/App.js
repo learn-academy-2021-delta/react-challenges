0ccb9848de20662a587b690b534e4f35f7518db0
Mayuridalavai <mayuri.gj@gmail.com>
2021-09-21 15:32:37 -0700

    completed first one

---
+++ intro-to-react/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class  Delta extends Component{
+  render(){
+    return (
+      <>
+      <h1>About us</h1>
+      <p>Anthony and Mayuri are students in Delta</p>
+      <footer>
+        US
+      </footer>
+      </>
+    )
+  }
+}
+
+export default Delta
