3bbff88a9daa60c5fbe43e4b02052fe17c8d335b
bluememfis <mrshaunsharp@gmail.com>
2021-09-21 16:24:27 -0700

    Our first react challenge

---
+++ react-challenges-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+class Content extends Component{
+  render(){
+    return(
+      <div>
+        <h1>We Chilling</h1>
+      </div>
+    )
+  }
+}
+
+export default Header
+export default Footer
+export default App;
---
+++ react-challenges-app/src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+    render() {
+      return (
+        <footer>
+          TeamHomies
+          <br />
+          Alpha 2020
+        </footer>
+      )
+    }
+  }
+  
+  export default Footer
