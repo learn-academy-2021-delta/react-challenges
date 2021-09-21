c2ae0c7cf4faa35392cc3fc9d8d1309589681aa5
pdisi <pdisidoro@yahoo.com>
2021-09-21 13:07:39 -1000

    Phil and Charlean challenge 1 progress

---
+++ intro-charlean-phil/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Header from './Component/Header'
+import Content from './Component/Content'
+import Footer from './Component/Footer'
+
+
+class App extends Component{
+  render(){
+    return (
+      <>
+      <h1></h1>
+      <Header/>
+      <Content/>
+      <Footer/>
+      </>
+    )
+  }
+}
+
+export default App
