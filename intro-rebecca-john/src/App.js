3d0e73fd75b6fcde84c00bf9a8acf5d6037d458d
JBrady <jbradysd@icloud.com>
2021-09-21 15:24:14 -0700

    challenge 1 complete

---
+++ intro-rebecca-john/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Delta extends Component {
+  render() {
+    return (
+      <>
+        <h1>About Us</h1>
+        <p>John has 2 barky chiuahas and Rebecca has two lovely cats</p>
+        <footer>Intro RB/JB Team</footer>
+      </>
+    )
+  }
+}
+
+export default Delta;
