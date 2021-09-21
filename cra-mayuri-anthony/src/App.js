28c5a3b989ff4c1f507c3340833099e3b851c216
Mayuridalavai <mayuri.gj@gmail.com>
2021-09-21 15:59:05 -0700

    First one completed

--- cra-mayuri-anthony/src/App.js
+++ cra-mayuri-anthony/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
-class  Delta extends Component{
+class Delta extends Component{
   render(){
     return (
       <>
       <h1>About us</h1>
-      <p>Anthony and Mayuri are students in Delta</p>
+      <p>Anthony and Mayuri are in Delta class</p>
       <footer>
         US
       </footer>
