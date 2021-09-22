756e0bba1fd4e2ae6953a1635216436e9912e809
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-09-22 16:27:28 -0700

    color box react ph1

---
+++ colorbox-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Color from './components/Color'
+import Square from './components/Square'
+
+      function App() {
+      return (
+        <div className="App">
+          <Color />
+          </div>
+        );
+      }
+      export default App;
+
+
+      
