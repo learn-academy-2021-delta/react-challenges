e3a9fe98cae4d0b65318d636d3fa9164f14d47e2
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-09-27 16:22:39 -0700

    props challeges

---
+++ no-bh-ks-app/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Tabs from "./Components/tabs";
+class App extends Component{
+  render(){
+    return (
+      <>
+      <title>testing</title>
+      <h1>About Us</h1>
+      <h2>Napoleon</h2>
+        <ul>
+        <li>Dunst</li>
+        </ul>
+        <h2>Brandon</h2>
+          <ul>
+          <li>Brain</li>
+          </ul>
+          <h2>Kevin</h2>
+            <ul>
+            <li>Muscle</li>
+            </ul>
+            <footer>
+            DZALMUS
+            </footer>
+<Tabs />
+            
+      </>
+    )
+  }
+}
+
+export default App
---
+++ props-napoleon-rebecca/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from `react`
+import './App.css';
+import Dice from './components/Dice'
+import Rolls from './components/Rolls'
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+
+    }
+  }
+
+
+
+  render(){
+    return(<>
+</>
+    )
+
+  }
+}
+export default App;
