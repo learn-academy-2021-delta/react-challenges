9218140a79e73a019ca606a1876b0d0e970afed1
Mayuridalavai <mayuri.gj@gmail.com>
2021-09-30 12:02:35 -0700

    updated challenge

--- react-input-ashley-mayuri/src/App.js
+++ react-input-ashley-mayuri/src/App.js
@@ -15,17 +15,20 @@ this.setState({userInput: e.target.value})
    render(){
    return (
      <>
-     <h1><u>Robo Active Listening</u></h1>
-<h3><u>Good Robot</u></h3>
-<p1>I hear you saying. Is that correct</p1>
-<h3><u>Bad Robot</u></h3>
-<p1>I hear you saying Bla. Is that correct?</p1>
-<h3><u>Kanyebot 5000</u></h3>
-<p1>I'm gunna let you finish but Beyonce is </p1>
+     < h1><u>Robo Active Listening</u></h1>
+     
      <input type = "text"
      value= {this.state.userInput}
-     onChange= {this.handleChange}/>
-       <h1>{this.state.userInput}</h1>
+     onChange= {this.handleChange}/> 
+    
+     <h3><u>Good Robot</u></h3>
+     <p1>I hear you saying "{this.state.userInput}". Is that correct</p1>
+     <h3><u>Bad Robot</u></h3>
+     <p1>I hear you saying {this.state.userInput.split("").map((value)=>{
+     if(value   === value) {return "BLA"}else{return value}})} Is that correct?</p1>
+     <h3><u>Kanyebot 5000</u></h3>
+     <p1>I'm gunna let you finish but Beyonce is "{this.state.userInput}".</p1> 
+    
      </>
     
    )
---
+++ react-input-ashley-mayuri/src/components/BadRobot.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BadRobot extends Component{
+    render(){
+        return(
+            <>
+             </>
+        )
+    }
+}
+export default BadRobot
